95dfb40aa82cbb6e233fbf285b72b64680a93a53
lazy-guy-1618 <95616943+lazy-guy-1618@users.noreply.github.com>
2024-03-01 19:16:24 +0530

    Update urls.py

--- Assign4/festmgmt/festmgmt/urls.py
+++ Assign4/festmgmt/festmgmt/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/student/', views.student_login_view, name='student_login'),
     path('volunteer/<int:event_id>/', views.volunteer_event, name='volunteer_event'),
     path('events/', views.events_view, name='events'),
+    path('login/organiser/', views.organiser_login_view, name='organiser_login'),
     # Add more URL patterns as needed
 ]

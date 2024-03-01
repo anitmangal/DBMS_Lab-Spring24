ce283a84fb347a4809f26ed829b5bd295d691fba
krishkhimasia <khimasiakrish@gmail.com>
2024-03-02 01:07:22 +0530

    global search/browse + participant can participate

--- Assign4/festmgmt/festmgmt/urls.py
+++ Assign4/festmgmt/festmgmt/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('login/student/', views.student_login_view, name='student_login'),
     path('volunteer/<int:event_id>/', views.volunteer_event, name='volunteer_event'),
     path('participate/<int:event_id>/', views.participate_student_event, name='participate_student_event'),
+    path('participate_event/<int:event_id>/', views.participate_event, name='participate_event'),
     path('events/', views.events_view, name='events'),
     path('login/organiser/', views.organiser_login_view, name='organiser_login'),
     path('admin/', admin.site.urls),
--- Assign4/festmgmt/festmgmt/views.py
+++ Assign4/festmgmt/festmgmt/views.py
@@ -13,6 +13,10 @@ def login_view(request):
     if request.user.is_authenticated:
         if request.user.is_superuser:
             return redirect('adminview')
+        if request.user.role == 'student':
+            return redirect('student_login')
+        elif request.user.role == 'organiser':
+            return redirect('organiser_login')
         return redirect('events')
     
     if request.method == 'POST':
@@ -56,7 +60,11 @@ def register_view(request):
 @login_required(login_url="student_login")
 def student_login_view(request):
     events = Event.objects.all()
+    timeslots = TimeSlot.objects.all()
+    venues = Venue.objects.all()
+    message = ''
     participate = Participates.objects.all()
+    
     
     curr_username = request.user
     student = Student.objects.get(username=curr_username)
@@ -74,8 +82,23 @@ def student_login_view(request):
                     participated_for_events.append(participated_event.event_id.event_id)
         except:
             pass    
-    return render(request, 'student_login.html', {'events': events, 'volunteered_for_events': volunteered_for_events, 'participated_for_events': participated_for_events})
-# ???? need to add logout
+        
+    query = request.GET.get('search')
+    search_type = request.GET.get('search_type')
+    if query is not None:
+        if search_type == 'event_name':
+            events = events.filter(event_name__icontains=query)
+        elif search_type == 'event_type':
+            events = events.filter(event_type__icontains=query)
+        elif search_type == 'event_description':
+            events = events.filter(event_description__icontains=query)
+        elif search_type == 'venue_name':
+            events = events.filter(venue_name__venue_name__icontains=query)
+        if not events.exists():
+            message = 'No such events!'
+
+    return render(request, 'student_login.html', {'events': events, 'timeslots': timeslots, 'venues': venues, 'message': message, 'query': query, 'search_type': search_type, 'volunteered_for_events': volunteered_for_events, 'participated_for_events': participated_for_events})
+
 def logout_view(request):
     logout(request)
     # Redirect to a logged-out page or home page
@@ -114,6 +137,26 @@ def participate_student_event(request, event_id):
     # Save the Participates object
     return redirect('student_login')
 
+@login_required(login_url='events')
+@transaction.atomic
+def participate_event(request, event_id):
+    event = Event.objects.get(pk=event_id)
+    curr_username = request.user
+    user_acc = useracc.objects.get(username=curr_username)
+    participant = Participant.objects.get(user_id=user_acc.user_id)
+
+    if Participates.objects.filter(participant_id=user_acc.user_id, event_id=event.event_id).exists():
+        return redirect('events')
+
+    # Create the Participates object
+    Participates.objects.create(
+        participant_id=participant,
+        event_id=event
+    )
+
+    return redirect('events')
+
+
 @login_required(login_url="organiser_login")
 def organiser_login_view(request):
     # Retrieve all events, timeslots, venues, and volunteers from the database
@@ -140,5 +183,35 @@ def organiser_login_view(request):
 
     return render(request, 'organiser_login.html', {'events': events, 'timeslots': timeslots, 'venues': venues, 'message': message, 'query': query, 'search_type': search_type})
 
+@login_required(login_url="events")
 def events_view(request):
-    return render(request, 'events.html')
+    events = Event.objects.all()
+    timeslots = TimeSlot.objects.all()
+    venues = Venue.objects.all()
+    message = ''
+    participate = Participates.objects.all()
+
+    curr_username = request.user
+    participant = Participant.objects.get(username=curr_username)
+    participated_for_events = []
+    if participant.is_authenticated:
+        for participated_event in participate:
+            if participated_event.participant_id.participant_id == participant.participant_id:                
+                participated_for_events.append(participated_event.event_id.event_id)
+
+
+    query = request.GET.get('search')
+    search_type = request.GET.get('search_type')
+    if query is not None:
+        if search_type == 'event_name':
+            events = events.filter(event_name__icontains=query)
+        elif search_type == 'event_type':
+            events = events.filter(event_type__icontains=query)
+        elif search_type == 'event_description':
+            events = events.filter(event_description__icontains=query)
+        elif search_type == 'venue_name':
+            events = events.filter(venue_name__venue_name__icontains=query)
+        if not events.exists():
+            message = 'No such events!'
+
+    return render(request, 'events.html', {'events': events, 'timeslots': timeslots, 'venues': venues, 'message': message, 'query': query, 'search_type': search_type, 'participated_for_events': participated_for_events})

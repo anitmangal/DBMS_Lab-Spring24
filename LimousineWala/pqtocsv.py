79f2a35089126a6f006823dd8b5c24a0e572dc19
krishkhimasia <khimasiakrish@gmail.com>
2024-04-13 15:47:49 +0530

    pq to csv file added

---
+++ LimousineWala/pqtocsv.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+rootfile='data/yellow_tripdata_2024-01'
+
+# Read Parquet file
+parquet_file = rootfile + '.parquet'
+df = pd.read_parquet(parquet_file)
+
+# Convert to CSV
+csv_file = rootfile + '.csv'
+df.to_csv(csv_file, index=False)
+
+print(f"Parquet file '{parquet_file}' has been converted to CSV file '{csv_file}'.")

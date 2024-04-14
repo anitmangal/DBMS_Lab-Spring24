2c667a37dd57bde0978e5b2f03431a5ff84950b0
optimus1010t <tanishqprasad1003@gmail.com>
2024-04-14 18:34:46 +0530

    taking just 10% from each dataset

--- Assign5/pqtocsv_dataset_concat.py
+++ Assign5/pqtocsv_dataset_concat.py
@@ -1,14 +1,11 @@
-
 import pandas as pd
 import os
+import random
 
 data_dir = './taxi'
 months = ['02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 
-file_name = 'yellow_tripdata_2024-01.parquet'
-file_path = os.path.join(data_dir, file_name)
-
-concatenated_data = pd.read_parquet(file_path)
+concatenated_data = pd.DataFrame()
 
 for month in months:
     file_name = f'yellow_tripdata_2023-{month}.parquet'
@@ -16,13 +13,31 @@ for month in months:
     
     if os.path.exists(file_path):
         data = pd.read_parquet(file_path)
-        concatenated_data = pd.concat([concatenated_data, data], ignore_index=True)
+        
+        num_samples = int(len(data) * 0.1)
+        sampled_data = data.sample(n=num_samples, random_state=42)
+        
+        concatenated_data = pd.concat([concatenated_data, sampled_data], ignore_index=True)
     else:
         print(f"File {file_name} not found.")
         
     print(f"Concatenating data for month {month}")
 
-output_file = './taxi/yellow_tripdata_2023_feb_to_dec_2024_jan.csv'
+file_name = 'yellow_tripdata_2024-01.parquet'
+file_path = os.path.join(data_dir, file_name)
+
+if os.path.exists(file_path):
+    data = pd.read_parquet(file_path)
+    num_samples = int(len(data) * 0.1)
+    sampled_data = data.sample(n=num_samples, random_state=42)
+    
+    concatenated_data = pd.concat([concatenated_data, sampled_data], ignore_index=True)
+else:
+    print(f"File {file_name} not found.")
+    
+print(f"Concatenating data for month {month}")
+    
+output_file = './taxi/yellow_tripdata_2023_feb_to_dec_2024_jan_10percent.csv'
 concatenated_data.to_csv(output_file, index=False)
 
-print(f'Concatenated data saved to {output_file}')
+print(f'Concatenated data (10% sampled) saved to {output_file}')
